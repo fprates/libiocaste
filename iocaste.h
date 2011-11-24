1bfa37350740c2a0e38d89608b2b68a618b4c08d
Francisco Prates <mad.kanie@gmail.com>
2011-11-24 17:42:07 -0200

    text_field: implementação de campo de entrada.

--- iocaste.h
+++ iocaste.h
@@ -11,6 +11,7 @@
 struct icst_element;
 struct icst_container;
 struct icst_text;
+struct icst_text_field;
 
 extern struct icst_container *ini_container(struct icst_container *super,
 		char *name);
@@ -19,6 +20,9 @@ extern struct icst_element *ini_element(struct icst_container *container,
 		char *name);
 
 extern struct icst_text *ini_text(struct icst_container *container, char *name);
+
+extern struct icst_text_field *ini_text_field(struct icst_container *container,
+		char *name);
 
 //struct icst_view_data;
 //struct icst_container;
---
+++ text_field.c
@@ -0,0 +1,29 @@
+/*
+ * text_field.c
+ *
+ *  Created on: 24/11/2011
+ *      Author: fprates
+ */
+
+#include "iocaste.h"
+#include "element.h"
+#include <stdlib.h>
+
+#define TEXT_FIELD "TEXT_FIELD"
+
+struct icst_text_field {
+	struct icst_element *super;
+};
+
+struct icst_text_field *ini_text_field(struct icst_container *container,
+		char *name)
+{
+	struct icst_text_field *tfield = malloc(sizeof(*tfield));
+
+	tfield->super = ini_element(container, name);
+	def_element_type(tfield->super, TEXT_FIELD);
+	def_element_ext(tfield->super, tfield);
+
+	return tfield;
+}
+
