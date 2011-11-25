6cb36889b1334ca746a08b21b57c84bbfd37c75b
Francisco Prates <mad.kanie@gmail.com>
2011-11-25 11:28:32 -0200

    text: reimplementação de text utilizando object.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- text.c
+++ text.c
@@ -6,22 +6,22 @@
  */
 
 #include "iocaste.h"
+#include "object.h"
 #include "element.h"
+#include "container.h"
 #include <stdlib.h>
 
 #define TEXT "TEXT"
 
 struct icst_text {
-	struct icst_element *super;
+	struct icst_object *this;
 };
 
 struct icst_text *ini_text(struct icst_container *container, char *name)
 {
 	struct icst_text *text = malloc(sizeof(*text));
 
-	text->super = ini_element(container, name);
-	def_element_type(text->super, TEXT);
-	def_element_ext(text->super, text);
+	text->this = ini_object(name, text, ini_element(container, name));
 
 	return text;
 }
