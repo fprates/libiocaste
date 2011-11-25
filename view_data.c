28360995aa930d270acd74d39db61bbc88471f5e
Francisco Prates <mad.kanie@gmail.com>
2011-11-25 21:20:06 -0200

    view_data: implementaçao de visao.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

---
+++ view_data.c
@@ -0,0 +1,23 @@
+/*
+ * view_data.c
+ *
+ *  Created on: 25/11/2011
+ *      Author: francisco.prates
+ */
+
+#include "iocaste.h"
+#include "object.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+struct icst_view_data {
+	struct icst_object *this;
+};
+
+struct icst_view_data *ini_view(char *view_name)
+{
+	struct icst_view_data *view = malloc(sizeof(*view));
+	view->this = ini_object(view_name, view, NULL);
+
+	return view;
+}
