5023a5b4e6333fb1278e41451e455c2619b51b21
Francisco Prates <mad.kanie@gmail.com>
2011-11-30 15:15:17 -0200

    object: todos as classes passam a ser encapsuladas por icst_object.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- iocaste.h
+++ iocaste.h
@@ -9,86 +9,58 @@
 #define IOCASTE_H_
 
 struct icst_object;
-struct icst_element {
-	struct icst_object *this;
-};
-
-struct icst_container {
-	struct icst_object *this;
-};
-
-struct icst_text {
-	struct icst_object *this;
-};
-
-struct icst_text_field {
-	struct icst_object *this;
-};
-
-struct icst_std_container {
-	struct icst_object *this;
-};
-
-struct icst_view_data {
-	struct icst_object *this;
-};
-
-struct icst_program {
-	struct icst_object *this;
-};
 
 struct icst_api {
-	struct icst_container *(*ini_container)(struct icst_container *super,
+	struct icst_object *(*ini_container)(struct icst_object *container,
 			char *name);
 
-	struct icst_element *(*ini_element)(struct icst_container *container,
+	struct icst_object *(*ini_element)(struct icst_object *container,
 			char *name);
 
-	struct icst_text *(*ini_text)(struct icst_container *container, char *name);
+	struct icst_object *(*ini_text)(struct icst_object *container, char *name);
 
-	struct icst_text_field *(*ini_text_field)(struct icst_container *container,
+	struct icst_object *(*ini_text_field)(struct icst_object *container,
 			char *name);
 
-	struct icst_std_container *(*ini_std_container)(
-			struct icst_container *container, char *name);
+	struct icst_object *(*ini_std_container)(struct icst_object *container,
+			char *name);
 
-	struct icst_program *(*ini_program)(char *name, char *prog_path,
+	struct icst_object *(*ini_program)(char *name, char *prog_path,
 			char *icst_path);
 
-	struct icst_view_data *(*ini_view)(struct icst_program *program,
+	struct icst_object *(*ini_view)(struct icst_object *program,
 			char *view_name);
 
-	void (*view_add)(struct icst_view_data *view,
-			struct icst_container *container);
+	void (*view_add)(struct icst_object *view, struct icst_object *container);
 
 	struct icst_object *(*super)(struct icst_object *object);
 };
 
-extern struct icst_container *icst_ini_container(
-		struct icst_container *container, char *name);
+extern struct icst_object *icst_ini_container(struct icst_object *container,
+		char *name);
 
-extern struct icst_element *icst_ini_element(struct icst_container *container,
+extern struct icst_object *icst_ini_element(struct icst_object *container,
 		char *name);
 
 extern struct icst_api *icst_ini(void);
 
-extern struct icst_std_container *icst_ini_std_container(
-		struct icst_container *container, char *name);
-
-extern struct icst_text *icst_ini_text(struct icst_container *container,
+extern struct icst_object *icst_ini_std_container(struct icst_object *container,
 		char *name);
 
-extern struct icst_text_field *icst_ini_text_field(
-		struct icst_container *container, char *name);
+extern struct icst_object *icst_ini_text(struct icst_object *container,
+		char *name);
 
-extern struct icst_program *icst_ini_program(char *name, char *prog_path,
+extern struct icst_object *icst_ini_text_field(struct icst_object *container,
+		char *name);
+
+extern struct icst_object *icst_ini_program(char *name, char *prog_path,
 		char *icst_path);
 
-extern struct icst_view_data *icst_ini_view(struct icst_program *program,
+extern struct icst_object *icst_ini_view(struct icst_object *program,
 		char *view_name);
 
-extern void icst_view_add(struct icst_view_data *view,
-		struct icst_container *container);
+extern void icst_view_add(struct icst_object *view,
+		struct icst_object *container);
 
 extern struct icst_object *icst_super(struct icst_object *object);
 
