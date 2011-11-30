3d64f36e2cde796bcbc8468f7d742ec629210ab3
Francisco Prates <mad.kanie@gmail.com>
2011-11-30 01:32:19 -0200

    text_field: reimplementa com exposição de "this".
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- text_field.c
+++ text_field.c
@@ -11,16 +11,17 @@
 
 #define TEXT_FIELD "TEXT_FIELD"
 
-struct icst_text_field {
-	struct icst_object *this;
+struct s_text_field {
+
 };
 
 struct icst_text_field *icst_ini_text_field(struct icst_container *container,
 		char *name)
 {
 	struct icst_text_field *tfield = malloc(sizeof(*tfield));
+	struct s_text_field *tfield_ = malloc(sizeof(*tfield_));
 
-	tfield->this = ini_object(name, tfield, icst_ini_element(container, name));
+	tfield->this = ini_object(name, tfield_, icst_ini_element(container, name));
 
 	return tfield;
 }
