fe7df6c2f6213e35086983ba38f7376641418362
Francisco Prates <mad.kanie@gmail.com>
2011-11-30 01:31:50 -0200

    std_container: reimplementa com exposição de "this".
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- std_container.c
+++ std_container.c
@@ -11,15 +11,16 @@
 
 #define STD_CONTAINER "STANDARD_CONTAINER"
 
-struct icst_std_container {
-	struct icst_object *this;
+struct s_std_container {
+
 };
 
 struct icst_std_container *icst_ini_std_container(
 		struct icst_container *container, char *name) {
 	struct icst_std_container *std_container = malloc(sizeof(*std_container));
+	struct s_std_container *std_container_ = malloc(sizeof(*std_container_));
 
-	std_container->this = ini_object(name, std_container,
+	std_container->this = ini_object(name, std_container_,
 			icst_ini_container(container, name));
 
 	return std_container;
